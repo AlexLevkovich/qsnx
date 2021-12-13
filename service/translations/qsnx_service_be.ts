01894480fee74434f81a55c93f7a2058281273d4
AlexLevkovich <alevkovich@tut.by>
2021-12-13 10:03:04 +0300

    update the translations

--- service/translations/qsnx_service_be.ts
+++ service/translations/qsnx_service_be.ts
@@ -4,7 +4,7 @@
 <context>
     <name>SNXProcess</name>
     <message>
-        <location filename="../qsnxservice.cpp" line="+118"/>
+        <location filename="../qsnxservice.cpp" line="+123"/>
         <source>Username and certificate file path cannot be both empty!</source>
         <translation type="unfinished">Імя карыстальніка і шлях да файла сертыфіката не могуць быць адначасова пустымі!</translation>
     </message>
@@ -17,9 +17,36 @@
 <context>
     <name>QSNXService</name>
     <message>
-        <location line="+77"/>
+        <location line="+78"/>
         <source> is already running. Stop or kill it first.</source>
-        <translation type="unfinished">ужо працуе. Спыніцеся або забіце яго першым.</translation>
+        <translation type="unfinished"> ужо працуе. Спыніцеся або забіце яго першым.</translation>
+    </message>
+</context>
+<context>
+    <name>KPtyDevice</name>
+    <message>
+        <location filename="../kptydevice.cpp" line="+463"/>
+        <location line="+14"/>
+        <source>Error opening PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location line="-200"/>
+        <source>Error reading from PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Error writing to PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+94"/>
+        <source>PTY operation timed out</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- service/translations/qsnx_service_ru.ts
+++ service/translations/qsnx_service_ru.ts
@@ -4,7 +4,7 @@
 <context>
     <name>SNXProcess</name>
     <message>
-        <location filename="../qsnxservice.cpp" line="+118"/>
+        <location filename="../qsnxservice.cpp" line="+123"/>
         <source>Username and certificate file path cannot be both empty!</source>
         <translation type="unfinished">Имя пользователя и путь к файлу сертификата не могут быть одновременно пустыми!</translation>
     </message>
@@ -17,9 +17,36 @@
 <context>
     <name>QSNXService</name>
     <message>
-        <location line="+77"/>
+        <location line="+78"/>
         <source> is already running. Stop or kill it first.</source>
-        <translation type="unfinished">уже работает. Сначала остановите или убейте его.</translation>
+        <translation type="unfinished"> уже работает. Сначала остановите или убейте его.</translation>
+    </message>
+</context>
+<context>
+    <name>KPtyDevice</name>
+    <message>
+        <location filename="../kptydevice.cpp" line="+463"/>
+        <location line="+14"/>
+        <source>Error opening PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location line="-200"/>
+        <source>Error reading from PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Error writing to PTY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+94"/>
+        <source>PTY operation timed out</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
